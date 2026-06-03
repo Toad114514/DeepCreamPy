f06cb3ce966437b731ac5e2c83641c28c042f355
Toad114514 <xiaolan2332021@163.com>
2026-06-04 03:32:14 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,9 @@ mkdir ./unpack
 unzip "./unpack/09-11-2019.DCPv2.model.zip" -d ./unpack
 mv "./unpack/09-11-2019 DCPv2 model" ./models
 
+# unpack Multisource
+unzip "./desencor_input/a.zip" -d ./desencor_input
+
 # start
 pip install -r requirements-cpu.txt
 python start_decbar.py
