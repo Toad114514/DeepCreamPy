ad56f66b1e0a3b8d020c834d29b05a93be9f92aa
Toad114514 <xiaolan2332021@163.com>
2026-06-04 03:56:17 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ unzip "./unpack/09-11-2019.DCPv2.model.zip" -d ./unpack
 mv "./unpack/09-11-2019 DCPv2 model" ./models
 
 # unpack Multisource
-unzip "./desencor_input/a.zip" -d ./desencor_input
+unzip "./decensor_input/a.zip" -d ./decensor_input
 
 # start
 pip install -r requirements-cpu.txt
