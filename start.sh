0742a53c4ebbda230d219d4306ccac1d3ce9a94e
Toad114514 <xiaolan2332021@163.com>
2026-06-04 03:24:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,7 @@
 
 # Models
 mkdir ./unpack
-curl -L -H "Authorization: token $MY_TOKEN" \
-  -o ./unpack/models.zip \
-  https://github.com/Toad114514/DeepCreamPy/releases/download/bar_models/09-11-2019.DCPv2.model.zip
-unzip "models.zip" -d ./unpack
+unzip "./unpack/09-11-2019.DCPv2.model.zip" -d ./unpack
 mv "./unpack/09-11-2019 DCPv2 model" ./models
 
 # start
