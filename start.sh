a59379fde14a312eddf6fb7654464321e262e7ff
Toad114514 <xiaolan2332021@163.com>
2026-06-04 02:55:48 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 # Action Custom script
 
 # Models
+mkdir ./unpack
 curl -L -H "Authorization: token $MY_TOKEN" \
   -o ./unpack/models.zip \
   https://github.com/Toad114514/DeepCreamPy/releases/download/bar_models/09-11-2019.DCPv2.model.zip
-mkdir unpack
 unzip "models.zip" -d ./unpack
 mv "./unpack/09-11-2019 DCPv2 model" ./models
 
 # start
 pip install -r requirements-cpu.txt
-python main.py
+python start_decbar.py
