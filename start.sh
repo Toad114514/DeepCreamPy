5b272c188681535cca878f0d28d2c92514e22b4f
Toad114514 <xiaolan2332021@163.com>
2026-06-05 01:00:42 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,6 +8,12 @@ mv "./unpack/09-11-2019 DCPv2 model/" ./models
 
 # unpack Multisource
 unzip "./decensor_input/a.zip" -d ./decensor_input
+unzip "./decensor_input/a2.zip" -d ./decensor_input
+unzip "./decensor_input/a3.zip" -d ./decensor_input
+unzip "./decensor_input/a4.zip" -d ./decensor_input
+unzip "./decensor_input/a5.zip" -d ./decensor_input
+unzip "./decensor_input/b.zip" -d ./decensor_input
+unzip "./decensor_input/c.zip" -d ./decensor_input
 
 # start
 pip install -r requirements-cpu.txt
